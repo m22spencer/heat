6622997aaee9061f414638d7445aceb526b1dae5
Matthew Spencer <m22spencer@gmail.com>
2013-11-11 03:21:53 -0600

    Add tests for CRLF/LF/Mixed endings.

--- Heat/test/TestCompletion.hx
+++ Heat/test/TestCompletion.hx
@@ -7,14 +7,50 @@ class TestCompletion extends TestCase {
     dir ("Project/Completion");
     assertProcessError( 1,
                         ~/^\[\]/,
-                        complete ("TestCompletionAndDoc.hx", 102, true));
+                        complete ("TestCompletionAndDocLF.hx", 102, true));
   }
   
-  public function testCompletionAndDoc() {
+  static var docRegex = ~/^\[{"name":"cpl","type":"Void \-> Unknown<0>","description":" Some documentation "/;
+  public function testCompletionAndDocLF_Char() {
     dir ("Project/Completion");
     assertProcessError( 0,
-                        ~/^\[{"name":"cpl","type":"Void \-> Unknown<0>","description":" Some documentation "/,
-                             complete ("TestCompletionAndDoc.hx", 106, true));
+                        docRegex,
+                        complete ("TestCompletionAndDocLF.hx", 106, true));
+  }
+
+  public function testCompletionAndDocCRLF_Char() {
+    dir ("Project/Completion");
+    assertProcessError( 0,
+                        docRegex,
+                        complete ("TestCompletionAndDocCRLF.hx", 106, true));
+  }
+
+  public function testCompletionAndDocMixed_Char() {
+    dir ("Project/Completion");
+    assertProcessError( 0,
+                        docRegex,
+                        complete ("TestCompletionAndDocMixed.hx", 106, true));
+  }
+
+  public function testCompletionAndDocLF_Byte() {
+    dir ("Project/Completion");
+    assertProcessError( 0,
+                        docRegex,
+                        complete ("TestCompletionAndDocLF.hx", 106, false));
+  }
+
+  public function testCompletionAndDocCRLF_Byte() {
+    dir ("Project/Completion");
+    assertProcessError( 0,
+                        docRegex,
+                        complete ("TestCompletionAndDocCRLF.hx", 111, false));
+  }
+
+  public function testCompletionAndDocMixed_Byte() {
+    dir ("Project/Completion");
+    assertProcessError( 0,
+                        docRegex,
+                        complete ("TestCompletionAndDocMixed.hx", 108, false));
   }
       
   //TODO: point vs byte handling
